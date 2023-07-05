3f950837f9a1a7761668046e76d5c95096e03e02
MarufiMac <marufalam120@gmail.com>
2023-07-05 17:56:29 +0600

    signup designed

--- app/src/main/java/com/example/decise/ui/HomeFragment.kt
+++ app/src/main/java/com/example/decise/ui/HomeFragment.kt
@@ -1,10 +1,5 @@
 package com.example.decise.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.decise.R
 import com.example.decise.base.BaseFragment
 import com.example.decise.databinding.FragmentHomeBinding
--- app/src/main/java/com/example/decise/ui/SignUpFragment.kt
+++ app/src/main/java/com/example/decise/ui/SignUpFragment.kt
@@ -1,5 +1,6 @@
 package com.example.decise.ui
 
+import androidx.appcompat.app.AppCompatActivity
 import com.example.decise.R
 import com.example.decise.base.BaseFragment
 import com.example.decise.databinding.FragmentSignUpBinding
@@ -8,6 +9,16 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
 
     override fun getFragmentView(): Int {
         return R.layout.fragment_sign_up
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
     }
 
 }
